3da6dbb63b9d3057bd75eb9b9dc76e8dcce86c4c
ansontrapani <anson.t.22315@gmail.com>
2026-01-04 23:10:10 -0500

    Removed more comments

--- src/logic.rs
+++ src/logic.rs
@@ -5,18 +5,4 @@ pub fn iced_date_to_local_datetime(date: Date) -> Result<chrono::NaiveDate, Stri
         Some(n) => Ok(n),
         None => Err(String::from("Invalid date")),
     }
-
-    // Switched this one to naive so no need for timezone accounting
-    // let naive_dt: chrono::NaiveDateTime = match naive_date
-    //     .and_hms_opt(0, 0, 0) {
-    //         Some(t) => t,
-    //         None => return Err(String::from("Invalid time"))
-    //     };
-
-    // match chrono::Local
-    //     .from_local_datetime(&naive_dt)
-    //     .single() {
-    //         Some(l) => Ok(l),
-    //         None => Err(String::from("ambiguous or invalid local datetime"))
-    //     }
 }
